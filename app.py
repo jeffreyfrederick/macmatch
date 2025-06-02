c4ce3444b096e8ef7cd0917e0894d2aeb57de820
Jeffrey Frederick <5095066+jeffreyfrederick@users.noreply.github.com>
2025-06-01 21:43:52 -0500

    add ipads

--- app.py
+++ app.py
@@ -89,5 +89,4 @@ def index():
     return render_template('index.html', grouped_results=grouped_results, query=query)
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=3000)
-     app.run(port=3000)
+    app.run(host='0.0.0.0', port=3000)
