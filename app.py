a1fdc1f48710acb5220116ecb3355fa9bd0827c7
Jeffrey Frederick <5095066+jeffreyfrederick@users.noreply.github.com>
2025-06-01 21:42:02 -0500

    add ipads

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 def load_data():
     data = []
-
     # load displays
     with open('displays.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
@@ -20,7 +19,6 @@ def load_data():
             entry = dict(zip(headers, row))
             entry['tag'] = 'MacBook Display'
             data.append(entry)
-
     # load macminis
     with open('macminis.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
@@ -34,7 +32,6 @@ def load_data():
             entry = dict(zip(headers, row))
             entry['tag'] = 'Mac Mini'
             data.append(entry)
-
     # load imacs
     with open('imacs.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
@@ -48,7 +45,6 @@ def load_data():
             entry = dict(zip(headers, row))
             entry['tag'] = 'iMac'
             data.append(entry)
-
     # load iphones
     with open('iphones.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
@@ -62,6 +58,19 @@ def load_data():
             entry = dict(zip(headers, row))
             entry['tag'] = 'iPhone'
             data.append(entry)
+    # load ipads
+    with open('ipads.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        headers = []
+        for row in reader:
+            if not row or row[0].startswith("!"):
+                continue
+            if not headers:
+                headers = row
+                continue
+            entry = dict(zip(headers, row))
+            entry['tag'] = 'iPad'
+            data.append(entry)
 
     return data
 
@@ -70,16 +79,15 @@ def index():
     query = ' '.join(request.form.get('query', '').strip().lower().split())
     data = load_data()
     grouped_results = defaultdict(list)
-
     if query:
         for row in data:
             searchable_values = [row.get(key, '').lower() for key in row]
             if any(query in value for value in searchable_values):
-            # Group by tag first, then color
                 group_key = (row['tag'], row.get('color', 'Unknown'))
                 grouped_results[group_key].append(row)
 
     return render_template('index.html', grouped_results=grouped_results, query=query)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+    # app.run(host='0.0.0.0', port=3000)
+     app.run(port=3000)
